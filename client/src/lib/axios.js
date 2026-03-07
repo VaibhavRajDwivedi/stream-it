dccfa98a93c98547660e73d335600809e8d7e710
VaibhavRajDwivedi <vaibhavrajdwivedi65@gmail.com>
2026-03-08 01:52:41 +0530

    fix api url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,8 @@
-// Bootstraps external networking module
 import axios from "axios";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: API_URL,
   withCredentials: true,
 });
